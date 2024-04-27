5992a69feb91e5f1a3542fc092345f9106a8fac7
Tomás Weigenast <tomaswegenast@gmail.com>
2024-04-27 16:32:57 -0300

    add mutex to network_notifier

--- network_notifier.go
+++ network_notifier.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 // network_notifier is a manager that creates new wires or attaches to them network interfaces
 type network_notifier struct {
+	mx    sync.RWMutex
 	wires map[string]*Wire
 }
 
@@ -17,6 +19,9 @@ var default_network_notifier *network_notifier = &network_notifier{wires: make(m
 //
 // the function gets the ip network and creates a new one for it if there are no other one
 func (nn *network_notifier) get_network_wire(network net.IPNet) *Wire {
+	nn.mx.Lock()
+	defer nn.mx.Unlock()
+
 	network_key := network.String()
 	wire, ok := nn.wires[network_key]
 	if !ok {
